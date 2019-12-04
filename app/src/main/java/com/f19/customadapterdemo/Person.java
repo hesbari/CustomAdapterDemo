52e59b80a3d5687735991fff1962f67a1792da03
Mohammad Kiani <kianimohammad@outlook.com>
2019-12-04 15:59:33 -0500

    Custom Adapter Demo

---
+++ app/src/main/java/com/f19/customadapterdemo/Person.java
@@ -0,0 +1,14 @@
+package com.f19.customadapterdemo;
+
+public class Person {
+
+    String name;
+    int id;
+    String email;
+
+    public Person(String name, int id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
